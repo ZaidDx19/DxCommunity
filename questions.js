644ce7ee360ffea81a52309f476a925476c1e5bf
ZaidDx19 <105630286+ZaidDx19@users.noreply.github.com>
2022-05-16 13:13:28 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is least?",
+    answer: "sin 7",
+    options: [
+      "sin 3",
+      "sin 2",
+      "sin 1",
+      "sin 7"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Number of solutions of sinx = x/10?",
+    answer: "7",
+    options: [
+      "5",
+      "7",
+      "8",
+      "6"
+    ]
+  },
+    {
+    numb: 3,
+    question: "If a circle same radius a and centres at (2,3) and (5,6) cut orthogonally, then it's equal to?",
+    answer: "3",
+    options: [
+      "6",
+      "4",
+      "3",
+      "10"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The circle passing throught the distinct points (1,t),(t,1) & (t,t) for all values of 't'>. Passes throught the point?",
+    answer: "(1,1)",
+    options: [
+      "(1,1)",
+      "(-1,-1)",
+      "(-1,1)",
+      "(1,-1)",
+    ]
+  },
+    {
+    numb: 5,
+    question: "How many of the 900 three digits number have at least one even digit?",
+    answer: "775",
+    options: [
+      "775",
+      "875",
+      "450",
+      "750"
+    ]
+  },
+  
+];
